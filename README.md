06c5d7ae03619d34bd08fba5210382d421f81c40
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-02-15 19:20:14 +0900

    refactor: class with asynchronous constructor design

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # kakao-gift-card
 🧧 OCR library for Kakao Gift Card
+
+## 📦 Installation
+
+```bash
+npm install kakao-gift-card
+# Or using yarn
+yarn add kakao-gift-card
+```
+
+## 🔥 Usage
+```typescript
+```
--- index.ts
+++ index.ts
@@ -1,45 +1,58 @@
-const fs = require('fs');
 const path = require('path');
 
-import { Worker, createWorker } from 'tesseract.js';
+import { Worker, ImageLike, createWorker } from 'tesseract.js';
 
-const worker = createWorker({
+const defaults = {
   langPath: path.join(__dirname, '../data', 'lang-data'),
-  logger: m => console.log(m),
-});
-
-const matchString = (string: string, regex: RegExp) => {
-  const matches = string.match(regex);
-  if (matches) {
-    return matches[0];
-  }
-  return '';
-}
-
-const getInfo = async (worker: Worker, filePath: string) => {
-  const { data: { text } } = await worker.recognize(fs.readFileSync(filePath));
-  console.log(text);
-  const lines = text.split('\n')
-    .filter((line) => line)
-    .map((line) => line.replace(/\s/g, ''));
-  console.log(lines);
-  const exchangerIndex = lines.findIndex((line) => line.includes('교환처'));
-  const barcode = parseInt(lines[exchangerIndex - 1]);
-  const product = lines[exchangerIndex - 2];
-
-  const date = new Date(matchString(text, /(\d{4}\.\d{2}\.\d{2})/g));
-  const order = parseInt(matchString(text, /(\d){8,}\w+/g));
-  return ({ product, barcode, date, order });
+  logger: (message: string) => console.log(message),
 };
 
-(async () => {
-  await worker.load();
-  await worker.loadLanguage('kor');
-  await worker.initialize('kor');
+export default class giftCardParser {
+  private worker: Worker;
+  public Ready: Promise<void>;
 
-  const chicken = await getInfo(worker, './chicken.jpeg');
-  const pepero = await getInfo(worker, './pepero.jpeg');
-  console.log(chicken, pepero);
+  constructor (
+    langPath: string = defaults.langPath,
+    logger: (message: string) => void = defaults.logger,
+  ) {
+    this.worker = createWorker({
+      langPath,
+      logger,
+    });
+    this.Ready = this.initialize();
+  }
 
-  await worker.terminate();
-})();
+  private async initialize() {
+    await this.worker.load();
+    await this.worker.loadLanguage('kor');
+    await this.worker.initialize('kor');
+  }
+
+  private matchString(string: string, regex: RegExp) {
+    const matches = string.match(regex);
+    if (matches) {
+      return matches[0];
+    }
+    return '';
+  }
+
+  public async getInfo(image: ImageLike) {
+    const { data: { text } } = await this.worker.recognize(image);
+
+    const lines = text.split('\n')
+      .filter((line: string) => line)
+      .map((line: string) => line.replace(/\s/g, ''));
+    const exchangerIndex = lines.findIndex((line) => line.includes('교환처'));
+
+    const barcode = parseInt(lines[exchangerIndex - 1]);
+    const product = lines[exchangerIndex - 2];
+
+    const date = new Date(this.matchString(text, /(\d{4}\.\d{2}\.\d{2})/g));
+    const order = parseInt(this.matchString(text, /(\d){8,}\w+/g));
+    return ({ product, barcode, date, order });
+  };
+
+  public async terminate() {
+    await this.worker.terminate();
+  }
+}
